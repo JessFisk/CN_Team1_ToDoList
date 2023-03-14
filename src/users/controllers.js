1e361baee8859056ba98beda03535313cf0f1f07
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-14 16:28:02 +0000

    added more folders for backend components. Rob wrote some code for the middleware/controller while livesharing too

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,3 +1,15 @@
 const User = require("./model");
 
 
+const registerUser = async (req, res) => {
+
+    try {
+
+        const user = await User.create(req.body);
+
+        res.status(201).json
+        
+    } catch (error) {
+        res.status(501).json({ errorMsg: error.message, error: error })
+    }
+}
