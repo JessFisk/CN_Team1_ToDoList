b673345169e8682c1f21c956cb9911efb60defcf
Robert Zdeb <robinio123@gmail.com>
2023-03-15 16:09:34 +0000

    add compare password method

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -18,30 +18,14 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 const comparePass = async (req, res, next) => {
     try {
-
         req.user = await User.findOne({ where: {username: req.body.username }});
-
         const match = await bcrypt.compare(req.body.password, req.user.password);
-
         if (!match) {
             const error = new Error("password do not match")
             res.status(500).json({ errorMessage: error.message, error: error });
-        }
-        
+        }   next();     
     } catch (error) {
         res.status(501).json({ errorMessage: error.message, error: error });
-    }
-}
+    }};
