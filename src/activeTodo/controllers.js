c9f79e0ae174517935250176c7e78a795b75f6e5
Jessica Fisk <fisk.jess@gmail.com>
2023-03-15 22:49:18 +0000

    delete Todo controller added

--- src/activeTodo/controllers.js
+++ src/activeTodo/controllers.js
@@ -9,7 +9,7 @@ const addActiveTodo = async (req, res) => {
             const error = new Error("User not authorised");
             res.status(401).json({ errorMessage: error.message, error: error });
         }
-        const newTodo = await AddTodo.create(req.body);
+        const newTodo = await AddTodo.create(req.body.todo);
         res.status(201).json({ message: "success", newTodo: newTodo });
     }
     catch (error) {
@@ -17,6 +17,37 @@ const addActiveTodo = async (req, res) => {
     }
 }
 
+
+const deleteActiveTodo = async (req, res) => {
+    try {
+        if (!req.authCheck) {
+            const error = new Error("User is not authorised");
+            res.status(401).json({ errorMessage: error.message, error: error });
+        }
+        const deleteTodo = await AddTodo.destroy({ where: { id: req.body.todo } });
+
+        if (deletedTodo >= 1) {
+            res.status(204).json({ message: "success", deletedTodo: deleteTodo });
+            return
+        } 
+        else {
+            res.status(404).json({ message: "Todo not found" });
+        }
+    }
+
+    catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+
+
+
+
+
+
+
 module.exports = {
     addActiveTodo,
-  };
+    deleteActiveTodo,
+};
