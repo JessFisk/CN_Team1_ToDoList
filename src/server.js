67311ce7d55932e6023970103d940669ef38e7f5
Jessica Fisk <fisk.jess@gmail.com>
2023-03-16 14:30:32 +0000

    Added Done Todo routes/controllers and model

--- src/server.js
+++ src/server.js
@@ -4,18 +4,27 @@ const cors = require("cors");
 const port = process.env.PORT || 5001
 
 const userRouter = require("./users/routes");
+const activeTodoRouter = require("./activeTodo/routes");
+const doneTodoRouter = require("./DoneTodo/routes");
+
 const User = require ("./users/model");
+const ActiveTodo = require ("./activeTodo/model");
+const DoneTodo = require ("./DoneTodo/model");
 
 const app = express();
 
 app.use(express.json());
 // app.use(cors());
 
-// const syncTables = () => {
-//     User.sync({alter:true, force:false})
-// }
+const syncTables = () => {
+    User.sync({alter:true, force:false});
+    ActiveTodo.sync();
+    DoneTodo.sync();
+}
 
 app.use(userRouter);
+app.use(activeTodoRouter)
+app.use(doneTodoRouter)
 
 app.get("/health", (req, res) => {
     res.status(200).json({message: "API is working. Yay"})
